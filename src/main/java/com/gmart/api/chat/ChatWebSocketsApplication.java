71fb62ee1c76c61a27634341d0b1c523bc859f3f
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2021-03-24 01:41:19 +0100

    Enable eureka client

--- src/main/java/com/gmart/api/chat/ChatWebSocketsApplication.java
+++ src/main/java/com/gmart/api/chat/ChatWebSocketsApplication.java
@@ -1,8 +1,9 @@
 package com.gmart.api.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class ChatWebSocketsApplication {
 
